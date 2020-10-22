fb097f06a82149a728e947db5975d91d38e4f77f
Deerop <xchenbox@gmail.com>
2020-10-21 18:14:07 -0700

    vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,5 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Test {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+public class Test extends LinearOpMode {
+    VuMarkNav nav = new VuMarkNav(hardwareMap, telemetry);
+
+    @Override
+    public void runOpMode() {
+        nav.activate();
+        waitForStart();
+        nav.deactivate();
+    }
 }
